5262881a778fd0003342d22211a9360e30cd15a4
Brandon Arias <2017030706@upsin.edu.mx>
2025-02-24 11:57:22 -0700

    Pantallas creadas

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,40 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const products = initialData.products;
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
-export default function ({ params }: Props) {
-  const { id } = params;
+export default async function ({ params }: Props) {
+  const { id } = await params;
 
-  if (id === "kids") {
-    notFound();
-  }
+  const category = products.filter((category) => category.gender === id);
+
+  const labels: Record<Category, string> = {
+    men: "para hombres",
+    women: "para mujeres",
+    kid: "para niños",
+    unisex: "para todos",
+  };
+
+  // if (id === "kids") {
+  //   notFound();
+  // }
 
   return (
-    <div>
-      <h1>Category Page</h1>
-    </div>
+    <>
+      <Title
+        title={`Articulos de ${labels[id]}`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+      <ProductGrid products={category} />
+    </>
   );
 }
