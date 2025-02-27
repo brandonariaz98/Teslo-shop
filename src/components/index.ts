c0f970f261d3526dd5fb49336881b6a659a48602
Brandon Arias <2017030706@upsin.edu.mx>
2025-02-26 21:51:35 -0700

    Part of the pagination added

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,7 @@ export * from "./product/size-selector/SizeSelector";
 export * from "./product/quantity-selector/QuantitySelector";
 
 export * from "./ui/footer/Footer";
+export * from "./ui/pagination/Pagination";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/sidebar/Sidebar";
 export * from "./ui/title/Title";
